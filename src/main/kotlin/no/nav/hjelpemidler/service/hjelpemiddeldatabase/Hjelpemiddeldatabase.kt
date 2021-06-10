ef851847bc9fa4f7eb8831e65ce272e267e50f6e
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-06-10 14:15:27 +0200

    Switched to hmdb endpoint "/alle" to get all data (historical too). Added elapsed time measurement for the hmdb download.

--- src/main/kotlin/no/nav/hjelpemidler/service/hjelpemiddeldatabase/Hjelpemiddeldatabase.kt
+++ src/main/kotlin/no/nav/hjelpemidler/service/hjelpemiddeldatabase/Hjelpemiddeldatabase.kt
@@ -11,31 +11,41 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 private val mapperJson = jacksonObjectMapper().registerModule(JavaTimeModule())
 
 private val logg = KotlinLogging.logger {}
 private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
+@ExperimentalTime
 class Hjelpemiddeldatabase {
     companion object {
         private var database: List<HjelpemiddelProdukt>? = null
 
         fun loadDatabase() {
             if (database == null) {
-                val apiURL = "${Configuration.application["HJELPEMIDDELDATABASEN_API"]!!}/produkter/alle-aktive-med-nav-avtale"
-                val client = HttpClient.newBuilder().build()
-                val request = HttpRequest.newBuilder()
-                    .uri(URI.create(apiURL))
-                    .timeout(Duration.ofSeconds(30))
-                    .header("Accepts", "application/json")
-                    .GET()
-                    .build()
-                val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
-                if (response.statusCode() == 200) {
-                    database = mapperJson.readValue(response.body().toString())
+                var statusCode = 0
+                val apiURL = "${Configuration.application["HJELPEMIDDELDATABASEN_API"]!!}/produkter/alle"
+                val elapsed: kotlin.time.Duration = measureTime {
+                    val client = HttpClient.newBuilder().build()
+                    val request = HttpRequest.newBuilder()
+                        .uri(URI.create(apiURL))
+                        .timeout(Duration.ofSeconds(30))
+                        .header("Accepts", "application/json")
+                        .GET()
+                        .build()
+                    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+                    statusCode = response.statusCode()
+                    if (statusCode == 200) {
+                        database = mapperJson.readValue(response.body().toString())
+                    }
+                }
+                if (statusCode == 200) {
+                    logg.info("Hjelpemiddel-database download successful: timeElapsed=${elapsed.inMilliseconds}ms url=$apiURL")
                 }else{
-                    logg.error("Unable to download the hjelpemiddel-database: statusCode=${response.statusCode()} url=$apiURL")
+                    logg.error("Unable to download the hjelpemiddel-database: statusCode=$statusCode url=$apiURL")
                 }
             }
         }
