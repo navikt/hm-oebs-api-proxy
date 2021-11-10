14ff7e66747333e6544b7e921365758a0ccdb01a
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2021-11-10 09:37:47 +0100

    Cleaned up old database stuff and rewrote isready/isalive to use hikari pool to check instead

--- src/main/kotlin/no/nav/hjelpemidler/Application.kt
+++ src/main/kotlin/no/nav/hjelpemidler/Application.kt
@@ -1,8 +1,6 @@
 package no.nav.hjelpemidler
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCall
 import io.ktor.application.call
@@ -26,7 +24,6 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.netty.EngineMain
-import io.ktor.util.KtorExperimentalAPI
 import io.micrometer.core.instrument.Clock
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
@@ -47,107 +44,20 @@ import no.nav.hjelpemidler.service.oebsdatabase.HjelpemiddeloversiktDao
 import no.nav.hjelpemidler.service.oebsdatabase.PersoninformasjonDao
 import no.nav.hjelpemidler.service.oebsdatabase.TittelForHmsnrDao
 import no.nav.hjelpemidler.serviceforespørsel.ServiceforespørselDao
-import oracle.jdbc.OracleConnection
-import oracle.jdbc.pool.OracleDataSource
 import org.slf4j.event.Level
-import java.sql.Connection
-import java.sql.SQLException
-import java.util.Properties
-import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.collections.set
 import kotlin.time.ExperimentalTime
 
 private val logg = KotlinLogging.logger {}
 private val sikkerlogg = KotlinLogging.logger("tjenestekall")
-
-private var dbConnection: Connection? = null
-private val ready = AtomicBoolean(false)
-
-private val mapperJson = jacksonObjectMapper().registerModule(JavaTimeModule())
 
 private val hjelpemiddeloversiktDao = HjelpemiddeloversiktDao()
 private val tittleForHmsnrDao = TittelForHmsnrDao()
 private val personinformasjonDao = PersoninformasjonDao()
 private val opprettServiceforespørselDao = ServiceforespørselDao()
 
-@ExperimentalTime
-fun main(args: Array<String>) {
-    logg.info("OEBS api proxy - Starting...")
-
-    logg.info("Connecting to OEBS-database")
-    connectToOebsDB()
-
-    // Serve http REST API requests
-    logg.info("Starting up ktor")
-    EngineMain.main(args)
-
-    // Note: We do not want to set ready=false again, dbConnection.isValid() will have kubernetes restart our entire pod as it is closed below.
-    // and setting ready=false forces our /isAlive-endpoint to always say "ALIVE".
-
-    // Cleanup
-    logg.info("OEBS api proxy - Cleaning up and stopping.")
-    dbConnection?.close()
-}
-
-fun connectToOebsDB() {
-    // Clean up resources if we have already had a database connection set up here that has now failed
-    ready.set(false)
-    dbConnection = null
-
-    // Set up a new connection
-    try {
-        sikkerlogg.info("Connecting to OEBS database with db-config-url=${Configuration.oracleDatabaseConfig["HM_OEBS_API_PROXY_DB_URL"]}, db-config-username=${Configuration.oracleDatabaseConfig["HM_OEBS_API_PROXY_DB_USR"]}")
-
-        // Set up database connection
-        val info = Properties()
-        info[OracleConnection.CONNECTION_PROPERTY_USER_NAME] =
-            Configuration.oracleDatabaseConfig["HM_OEBS_API_PROXY_DB_USR"]!!
-        info[OracleConnection.CONNECTION_PROPERTY_PASSWORD] =
-            Configuration.oracleDatabaseConfig["HM_OEBS_API_PROXY_DB_PW"]!!
-        info[OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH] = "20"
-
-        val ods = OracleDataSource()
-        ods.url = Configuration.oracleDatabaseConfig["HM_OEBS_API_PROXY_DB_URL"]!!
-        ods.connectionProperties = info
-
-        logg.info("Connecting to database")
-        dbConnection = ods.getConnection()
-
-        logg.info("Fetching db metadata")
-        val dbmd = dbConnection!!.metaData
-        logg.info("Driver Name: " + dbmd.driverName)
-        logg.info("Driver Version: " + dbmd.driverVersion)
-
-        logg.info("Database connected, hm-oebs-api-proxy ready")
-        ready.set(true)
-    } catch (e: Exception) {
-        logg.info("Exception while connecting to database: $e")
-        e.printStackTrace()
-        throw e
-    }
-}
-
-// Meant to fix "java.sql.SQLException: ORA-02399: overskred maks. tilkoblingstid, du blir logget av", by reconnection to the database and retrying
-fun <T> withRetryIfDatabaseConnectionIsStale(block: () -> T): T {
-    var lastException: SQLException? = null
-    for (attempt in 1..3) { // We get three attempts
-        try {
-            return block() // Success
-        } catch (e: SQLException) {
-            lastException = e
-            if (e.toString().contains("ORA-02399")) {
-                logg.warn("Oracle database closed the connection due to their connection-max-life deadline, we reconnect and try again: $e")
-                connectToOebsDB() // Reset database connection
-                continue // Retry if we have attempts left
-            }
-            throw e // Unhandled sql error, we throw up
-        }
-    }
-    throw lastException!! // No more attempts so we throw the last exception we had
-}
+fun main(args: Array<String>) = EngineMain.main(args)
 
 @ExperimentalTime
-@KtorExperimentalAPI
 fun Application.module() {
     installAuthentication()
 
@@ -159,9 +69,9 @@ fun Application.module() {
         level = Level.TRACE
         filter { call ->
             !call.request.path().startsWith("/internal") &&
-                !call.request.path().startsWith("/isalive") &&
-                !call.request.path().startsWith("/isready") &&
-                !call.request.path().startsWith("/metrics")
+                    !call.request.path().startsWith("/isalive") &&
+                    !call.request.path().startsWith("/isready") &&
+                    !call.request.path().startsWith("/metrics")
         }
     }
 
@@ -186,24 +96,20 @@ fun Application.module() {
         // Endpoints for Kubernetes unauthenticated health checks
 
         get("/isalive") {
-            // If we have gotten ready=true we check that dbConnection is still valid, or else we are ALIVE (so we don't get our pod restarted during startup)
-            if (ready.get()) {
-                val dbValid = dbConnection!!.isValid(10)
-                if (!dbValid) {
-                    Prometheus.oebsDbAvailable.set(0.0)
-                    return@get call.respondText("NOT ALIVE", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
-                }
-                Prometheus.oebsDbAvailable.set(1.0)
+            // Let's check if the datasource has been closed
+            if (Configuration.dataSource.isClosed) {
+                return@get call.respondText("NOT ALIVE", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
             }
             call.respondText("ALIVE", ContentType.Text.Plain)
         }
 
         get("/isready") {
-            if (!ready.get()) return@get call.respondText(
-                "NOT READY",
-                ContentType.Text.Plain,
-                HttpStatusCode.ServiceUnavailable
-            )
+            // Let's check if the datasource is still valid and working
+            if (!Configuration.dataSource.getConnection().isValid(10)) {
+                Prometheus.oebsDbAvailable.set(0.0)
+                return@get call.respondText("NOT READY", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
+            }
+            Prometheus.oebsDbAvailable.set(1.0)
             call.respondText("READY", ContentType.Text.Plain)
         }
 
