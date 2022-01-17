f65c88eac7ab3bccfb3c2be25963e4355be0b528
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-01-17 16:27:31 +0100

    Brukerpass test for produksjonssettingen

--- src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
+++ src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
@@ -10,6 +10,7 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import mu.KotlinLogging
 import no.nav.hjelpemidler.configuration.Configuration
+import no.nav.hjelpemidler.service.oebsdatabase.BrukerpassDao
 import no.nav.hjelpemidler.service.oebsdatabase.HjelpemiddeloversiktDao
 import no.nav.hjelpemidler.service.oebsdatabase.TittelForHmsnrDao
 
@@ -18,6 +19,8 @@ private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
 private val hjelpemiddeloversiktDao = HjelpemiddeloversiktDao()
 private val tittleForHmsnrDao = TittelForHmsnrDao()
+
+private val brukerpassDao = BrukerpassDao()
 
 fun Route.hjelpemiddelsiden() {
     // Authenticated database proxy requests
@@ -35,6 +38,24 @@ fun Route.hjelpemiddelsiden() {
             if (!"\\d{11}".toRegex().matches(fnr)) {
                 error("invalid fnr in 'pid', does not match regex")
             }
+
+            // TODO: START:
+            if (Configuration.application["APP_PROFILE"]!! != "prod") {
+                logg.info("Processing request for /brukerpass (on-behalf-of: $fnr)")
+            } else {
+                logg.info("Processing request for /brukerpass")
+            }
+            // Extra sanity check of FNR
+            if (!"\\d{11}".toRegex().matches(fnr)) {
+                error("invalid fnr in 'pid', does not match regex")
+            }
+            val bp = brukerpassDao.brukerpassForFnr(fnr)
+            data class Result(
+                val fantBruker: Boolean,
+                val brukerpass: Boolean,
+            )
+            logg.info("DEBUG TEST: brukerpass-resultat: $bp")
+            // TODO: END:
 
             call.respond(hjelpemiddeloversiktDao.hentHjelpemiddeloversikt(fnr))
         }
