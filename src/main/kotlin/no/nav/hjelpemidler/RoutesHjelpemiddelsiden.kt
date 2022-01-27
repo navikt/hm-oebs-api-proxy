8c1a8e6948ec7c29e320585935fea44840a74b3f
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-01-27 09:48:57 +0100

    Added test code back for new api version

--- src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
+++ src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
@@ -10,14 +10,18 @@ import io.ktor.routing.get
 import io.ktor.routing.post
 import mu.KotlinLogging
 import no.nav.hjelpemidler.configuration.Configuration
+import no.nav.hjelpemidler.service.oebsdatabase.Brukerpass
+import no.nav.hjelpemidler.service.oebsdatabase.BrukerpassDao
 import no.nav.hjelpemidler.service.oebsdatabase.HjelpemiddeloversiktDao
 import no.nav.hjelpemidler.service.oebsdatabase.TittelForHmsnrDao
+import kotlin.system.measureTimeMillis
 
 private val logg = KotlinLogging.logger {}
 private val sikkerlogg = KotlinLogging.logger("tjenestekall")
 
 private val hjelpemiddeloversiktDao = HjelpemiddeloversiktDao()
 private val tittleForHmsnrDao = TittelForHmsnrDao()
+private val brukerpassDao = BrukerpassDao()
 
 fun Route.hjelpemiddelsiden() {
     // Authenticated database proxy requests
@@ -35,6 +39,23 @@ fun Route.hjelpemiddelsiden() {
             if (!"\\d{11}".toRegex().matches(fnr)) {
                 error("invalid fnr in 'pid', does not match regex")
             }
+
+            // TODO: START:
+            var res: Brukerpass? = null
+            val timeElapsed = measureTimeMillis {
+                if (Configuration.application["APP_PROFILE"]!! != "prod") {
+                    logg.info("Processing request for /brukerpass (on-behalf-of: $fnr)")
+                } else {
+                    logg.info("Processing request for /brukerpass")
+                }
+                // Extra sanity check of FNR
+                if (!"\\d{11}".toRegex().matches(fnr)) {
+                    error("invalid fnr in 'pid', does not match regex")
+                }
+                res = brukerpassDao.brukerpassForFnr(fnr)
+            }
+            logg.info("DEBUG TEST: brukerpass-resultat: $res (timeElapsed=${timeElapsed}ms)")
+            // TODO: END:
 
             call.respond(hjelpemiddeloversiktDao.hentHjelpemiddeloversikt(fnr))
         }
