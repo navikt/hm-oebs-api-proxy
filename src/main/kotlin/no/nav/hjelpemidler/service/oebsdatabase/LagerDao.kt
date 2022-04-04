463adb1b0f8271d500e9ddc4a92b41b0bc556019
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-04-04 10:40:34 +0200

    LagerDao() for å snakke med ny tabell for lager-status, inkl. test api

---
+++ src/main/kotlin/no/nav/hjelpemidler/service/oebsdatabase/LagerDao.kt
@@ -0,0 +1,104 @@
+package no.nav.hjelpemidler.service.oebsdatabase
+
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import mu.KotlinLogging
+import no.nav.hjelpemidler.configuration.Configuration
+import org.intellij.lang.annotations.Language
+import javax.sql.DataSource
+
+private val logg = KotlinLogging.logger {}
+
+class LagerDao(private val dataSource: DataSource = Configuration.dataSource) {
+    fun lagerStatus(hmsnr: String): List<LagerStatus> {
+        @Language("OracleSQL")
+        var query =
+            """
+                SELECT
+                    organisasjons_id,
+                    organisasjons_navn,
+                    artikkelnummer,
+                    artikkelid,
+                    fysisk,
+                    tilgjengeligatt,
+                    tilgjengeligroo,
+                    tilgjengelig,
+                    behovsmeldt,
+                    reservert,
+                    restordre,
+                    bestillinger,
+                    anmodning,
+                    intanmodning,
+                    forsyning,
+                    sortiment,
+                    lagervare,
+                    minmax
+                FROM XXRTV_DIGIHOT_UTVID_ART_SOK_V
+                WHERE
+                    artikkelnummer = ?
+                ;
+            """.trimIndent()
+
+        return sessionOf(dataSource).use { it ->
+            it.run(
+                queryOf(query, hmsnr).map { row ->
+                    testHelper(row)
+                    LagerStatus(
+                        erPåLager = (
+                            row.int("fysisk") +
+                                row.int("bestillinger") +
+                                row.int("anmodning") +
+                                row.int("intanmodning")
+                            ) >= (
+                            + row.int("behovsmeldt") +
+                                row.int("reservert") +
+                                row.int("restordre")
+                            ),
+
+                        organisasjons_id = row.int("organisasjons_id"),
+                        organisasjons_navn = row.string("organisasjons_navn"),
+                        artikkelnummer = row.string("artikkelnummer"),
+                        artikkelid = row.int("artikkelid"),
+                        fysisk = row.int("fysisk"),
+                        tilgjengeligatt = row.int("tilgjengeligatt"),
+                        tilgjengeligroo = row.int("tilgjengeligroo"),
+                        tilgjengelig = row.int("tilgjengelig"),
+                        behovsmeldt = row.int("behovsmeldt"),
+                        reservert = row.int("reservert"),
+                        restordre = row.int("restordre"),
+                        bestillinger = row.int("bestillinger"),
+                        anmodning = row.int("anmodning"),
+                        intanmodning = row.int("intanmodning"),
+                        forsyning = row.int("forsyning"),
+                        sortiment = row.boolean("sortiment"),
+                        lagervare = row.boolean("lagervare"),
+                        minmax = row.boolean("minmax"),
+                    )
+                }.asList
+            )
+        }
+    }
+}
+
+data class LagerStatus(
+    val erPåLager: Boolean,
+
+    val organisasjons_id: Int,
+    val organisasjons_navn: String,
+    val artikkelnummer: String,
+    val artikkelid: Int,
+    val fysisk: Int,
+    val tilgjengeligatt: Int,
+    val tilgjengeligroo: Int,
+    val tilgjengelig: Int,
+    val behovsmeldt: Int,
+    val reservert: Int,
+    val restordre: Int,
+    val bestillinger: Int,
+    val anmodning: Int,
+    val intanmodning: Int,
+    val forsyning: Int,
+    val sortiment: Boolean,
+    val lagervare: Boolean,
+    val minmax: Boolean,
+)
