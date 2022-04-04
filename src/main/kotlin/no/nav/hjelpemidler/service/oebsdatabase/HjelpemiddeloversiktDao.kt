2a9b42c95f3b84cb37ce6feb6ae49c0ff1760af2
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2022-04-04 13:11:20 +0200

    Forbered lansering av nytt felt-navn i produksjon: FØRSTE_UTSENDELSE -> UTLÅNS_DATO

--- src/main/kotlin/no/nav/hjelpemidler/service/oebsdatabase/HjelpemiddeloversiktDao.kt
+++ src/main/kotlin/no/nav/hjelpemidler/service/oebsdatabase/HjelpemiddeloversiktDao.kt
@@ -13,76 +13,35 @@ import javax.sql.DataSource
 
 class HjelpemiddeloversiktDao(private val dataSource: DataSource = Configuration.dataSource) {
     fun hentHjelpemiddeloversikt(fnr: String): List<HjelpemiddelBruker> {
+        @Language("OracleSQL")
+        val query =
+            """
+            SELECT ANTALL, ENHET, KATEGORI3_BESKRIVELSE, ARTIKKEL_BESKRIVELSE, ARTIKKELNUMMER, 
+                   SERIE_NUMMER, UTLÅNS_DATO, ORDRE_NUMMER, KATEGORI3_NUMMER, ARTIKKELSTATUS  
+            FROM XXRTV_DIGIHOT_HJM_UTLAN_FNR_V
+            WHERE FNR = ?
+            ORDER BY UTLÅNS_DATO DESC
+            """.trimIndent()
 
-        /*
-        * Team OEBS har endret navn på kolonnen "FØRSTE_UTSENDELSE". Det er kun gjort i Q1 og ikke prod enda.
-        * Når det er gjort i prod, kan denne ifen fjernes
-        * */
-        if (System.getenv().getValue("NAIS_CLUSTER_NAME") == "prod-fss") {
-            log.info("Fallbacker til å spørre på FØRSTE_UTSENDELSE i prod")
-
-            @Language("OracleSQL")
-            val query =
-                """
-                SELECT ANTALL, ENHET, KATEGORI3_BESKRIVELSE, ARTIKKEL_BESKRIVELSE, ARTIKKELNUMMER, 
-                       SERIE_NUMMER, FØRSTE_UTSENDELSE, ORDRE_NUMMER, KATEGORI3_NUMMER, ARTIKKELSTATUS  
-                FROM XXRTV_DIGIHOT_HJM_UTLAN_FNR_V
-                WHERE FNR = ?
-                ORDER BY FØRSTE_UTSENDELSE DESC
-                """.trimIndent()
-
-            val items = sessionOf(dataSource).use {
-                it.run(
-                    queryOf(query, fnr).map { row ->
-                        HjelpemiddelBruker(
-                            antall = row.string("ANTALL"),
-                            antallEnhet = row.string("ENHET"),
-                            kategoriNummer = row.string("KATEGORI3_NUMMER"),
-                            kategori = row.string("KATEGORI3_BESKRIVELSE"),
-                            artikkelBeskrivelse = row.string("ARTIKKEL_BESKRIVELSE"),
-                            artikkelNr = row.string("ARTIKKELNUMMER"),
-                            serieNr = row.stringOrNull("SERIE_NUMMER"),
-                            datoUtsendelse = row.string("FØRSTE_UTSENDELSE"),
-                            ordrenummer = row.stringOrNull("ORDRE_NUMMER"),
-                            artikkelStatus = row.string("ARTIKKELSTATUS")
-                        )
-                    }.asList
-                )
-            }
-            return berikOrdrelinjer(items)
-        } else {
-            log.info("Spør på UTLÅNS_DATO i alle miljøer som ikke er prod")
-
-            @Language("OracleSQL")
-            val query =
-                """
-                SELECT ANTALL, ENHET, KATEGORI3_BESKRIVELSE, ARTIKKEL_BESKRIVELSE, ARTIKKELNUMMER, 
-                       SERIE_NUMMER, UTLÅNS_DATO, ORDRE_NUMMER, KATEGORI3_NUMMER, ARTIKKELSTATUS  
-                FROM XXRTV_DIGIHOT_HJM_UTLAN_FNR_V
-                WHERE FNR = ?
-                ORDER BY UTLÅNS_DATO DESC
-                """.trimIndent()
-
-            val items = sessionOf(dataSource).use {
-                it.run(
-                    queryOf(query, fnr).map { row ->
-                        HjelpemiddelBruker(
-                            antall = row.string("ANTALL"),
-                            antallEnhet = row.string("ENHET"),
-                            kategoriNummer = row.string("KATEGORI3_NUMMER"),
-                            kategori = row.string("KATEGORI3_BESKRIVELSE"),
-                            artikkelBeskrivelse = row.string("ARTIKKEL_BESKRIVELSE"),
-                            artikkelNr = row.string("ARTIKKELNUMMER"),
-                            serieNr = row.stringOrNull("SERIE_NUMMER"),
-                            datoUtsendelse = row.string("UTLÅNS_DATO"),
-                            ordrenummer = row.stringOrNull("ORDRE_NUMMER"),
-                            artikkelStatus = row.string("ARTIKKELSTATUS")
-                        )
-                    }.asList
-                )
-            }
-            return berikOrdrelinjer(items)
+        val items = sessionOf(dataSource).use {
+            it.run(
+                queryOf(query, fnr).map { row ->
+                    HjelpemiddelBruker(
+                        antall = row.string("ANTALL"),
+                        antallEnhet = row.string("ENHET"),
+                        kategoriNummer = row.string("KATEGORI3_NUMMER"),
+                        kategori = row.string("KATEGORI3_BESKRIVELSE"),
+                        artikkelBeskrivelse = row.string("ARTIKKEL_BESKRIVELSE"),
+                        artikkelNr = row.string("ARTIKKELNUMMER"),
+                        serieNr = row.stringOrNull("SERIE_NUMMER"),
+                        datoUtsendelse = row.string("UTLÅNS_DATO"),
+                        ordrenummer = row.stringOrNull("ORDRE_NUMMER"),
+                        artikkelStatus = row.string("ARTIKKELSTATUS")
+                    )
+                }.asList
+            )
         }
+        return berikOrdrelinjer(items)
     }
 
     private fun berikOrdrelinjer(items: List<HjelpemiddelBruker>): List<HjelpemiddelBruker> = runBlocking {
