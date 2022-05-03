405fa2a1ebe0ef3f68bea85f5873642721d8d0e3
Gustav Lillesæter <gustav.lillesater@nav.no>
2022-05-03 13:31:55 +0200

    use port 1521 for t1 connection

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 if [ -d "/secrets/oebst1" ]; then
     export HM_OEBS_API_PROXY_DB_USR_T1=$(cat /secrets/oebst1/credentials/username)
     export HM_OEBS_API_PROXY_DB_PW_T1=$(cat /secrets/oebst1/credentials/password)
-    export HM_OEBS_API_PROXY_DB_URL_T1=jdbc:oracle:thin:@d26dbfl027.test.local:1521/oebst1fi
+    export HM_OEBS_API_PROXY_DB_URL_T1=jdbc:oracle:thin:@d26dbfl027.test.local:1521/oebst1
+fi
 
 if [ -d "/secrets/oebsq1" ]; then
     export HM_OEBS_API_PROXY_DB_USR_Q1=$(cat /secrets/oebsq1/credentials/username)
