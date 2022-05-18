d605ac5a46b186368c56ed957d8279060df60cd8
Erik Eiesland <erik@eies.land>
2022-05-18 13:46:18 +0200

    fikser headers i kall til oebs

--- src/main/kotlin/no/nav/hjelpemidler/client/oebs/OebsApiClient.kt
+++ src/main/kotlin/no/nav/hjelpemidler/client/oebs/OebsApiClient.kt
@@ -4,14 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.accept
 import io.ktor.client.request.bearerAuth
-import io.ktor.client.request.headers
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.serialization.jackson.jackson
@@ -61,11 +60,9 @@ class OebsApiClient(engine: HttpClientEngine) {
         bestilling: Ordre,
     ): HttpResponse {
         return client.post(apiUrl) {
-            headers {
-                contentType(ContentType.Application.Json)
-                append(HttpHeaders.Accept, "application/json")
-                bearerAuth(apiToken)
-            }
+            bearerAuth(apiToken)
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
             setBody(bestilling)
         }
     }
