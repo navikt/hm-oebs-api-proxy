00db44744fb1b1079a310c7cdd7dece27380a792
Håkon <hakonhagen2@gmail.com>
2023-09-19 09:52:22 +0200

    sjekk bydel

--- src/main/kotlin/no/nav/hjelpemidler/service/oebsdatabase/TestDao.kt
+++ src/main/kotlin/no/nav/hjelpemidler/service/oebsdatabase/TestDao.kt
@@ -20,7 +20,7 @@ class TestDao(private val dataSource: DataSource = Configuration.dataSource) {
          testArtBeskrivelse()
          testBrukerpass()
          testLagerstatus()
-//        testPersoninformasjon()
+         testPersoninformasjon()
     }
 
     private fun testSelectAll() {
@@ -167,7 +167,7 @@ class TestDao(private val dataSource: DataSource = Configuration.dataSource) {
             """
                 SELECT *
                 FROM apps.XXRTV_DIGIHOT_OEBS_ADR_FNR_V
-                WHERE FNR = '27066427779'
+                WHERE BYDEL IS NOT NULL
             """.trimIndent()
 
         val result = sessionOf(dataSource).use {
