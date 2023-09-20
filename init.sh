7efd1d3b6599efdfc395638d7daa7176a398e51f
Ole Steinar Lillestøl Skrede <Ole.Steinar.Lillestol.Skrede@nav.no>
2023-09-20 13:28:52 +0200

    samme creds for q2 som i q1

--- init.sh
+++ init.sh
@@ -11,8 +11,10 @@ if [ -d "/secrets/oebsq1" ]; then
 fi
 
 if [ -d "/secrets/oebsq2" ]; then
-    export HM_OEBS_API_PROXY_DB_USR_Q2=$(cat /secrets/oebsq2/credentials/username)
-    export HM_OEBS_API_PROXY_DB_PW_Q2=$(cat /secrets/oebsq2/credentials/password)
+    # samme credentials som i q1
+    export HM_OEBS_API_PROXY_DB_USR_Q2=$(cat /secrets/oebsq1/credentials/username)
+    export HM_OEBS_API_PROXY_DB_PW_Q2=$(cat /secrets/oebsq1/credentials/password)
+    # jdbc_url ligger i kubernetes secret
 #    export HM_OEBS_API_PROXY_DB_URL_Q2=$(cat /secrets/oebsq2/config/jdbc_url)
 fi
 
