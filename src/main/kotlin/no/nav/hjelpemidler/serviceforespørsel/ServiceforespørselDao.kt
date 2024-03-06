525e8d63a0c6115052392965f0c9227e43ff04ff
Erik Eiesland <erik.eiesland@nav.no>
2024-03-06 09:08:25 +0100

    tester med tom string i stedet for null som beskrivelse

--- src/main/kotlin/no/nav/hjelpemidler/serviceforespørsel/ServiceforespørselDao.kt
+++ src/main/kotlin/no/nav/hjelpemidler/serviceforespørsel/ServiceforespørselDao.kt
@@ -13,11 +13,11 @@ class ServiceforespørselDao(private val dataSource: DataSource = Configuration.
         @Language("Oracle")
         val opprettSFQuery =
             """
-                insert into apps.xxrtv_cs_digihot_sf_opprett
+                INSERT INTO apps.xxrtv_cs_digihot_sf_opprett
                 (ID, FNR, NAVN, STONADSKLASS, SAKSTYPE, RESULTAT, SFDATO, REFERANSENUMMER, KILDE, PROCESSED, LAST_UPDATE_DATE,
                  LAST_UPDATED_BY, CREATION_DATE, CREATED_BY, JOB_ID, SAKSBLOKK, BESKRIVELSE)
-                values (apps.XXRTV_CS_DIGIHOT_SF_OPPRETT_S.nextval, :fnr, :navn, :stonadsklasse, :sakstype, :resultat, sysdate,
-                :referansenummer, :kilde, :processed, sysdate, :oppdatertAv, sysdate, :oppdatertAv, :jobId, 'X', :beskrivelse)
+                VALUES (apps.XXRTV_CS_DIGIHOT_SF_OPPRETT_S.nextval, :fnr, :navn, :stonadsklasse, :sakstype, :resultat, SYSDATE,
+                        :referansenummer, :kilde, :processed, SYSDATE, :oppdatertAv, SYSDATE, :oppdatertAv, :jobId, 'X', :beskrivelse)
             """.trimIndent()
 
         sessionOf(dataSource).use {
@@ -35,7 +35,7 @@ class ServiceforespørselDao(private val dataSource: DataSource = Configuration.
                         "processed" to "N",
                         "oppdatertAv" to Configuration.application["OEBS_BRUKER_ID"],
                         "jobId" to -1,
-                        "beskrivelse" to sf.problemsammendrag,
+                        "beskrivelse" to (sf.problemsammendrag ?: ""),
                     ),
                 ).asUpdate,
             )
