dd532b05d4a767850596a206fa31b8a562009d5e
Erik Eiesland <erik.eiesland@nav.no>
2024-03-07 14:17:01 +0100

    tillater datoUtsendelse = null

--- src/main/kotlin/no/nav/hjelpemidler/models/HjelpemiddelBruker.kt
+++ src/main/kotlin/no/nav/hjelpemidler/models/HjelpemiddelBruker.kt
@@ -8,7 +8,7 @@ data class HjelpemiddelBruker(
     val artikkelBeskrivelse: String,
     val artikkelNr: String,
     val serieNr: String?,
-    val datoUtsendelse: String,
+    val datoUtsendelse: String?,
     val ordrenummer: String?,
     val artikkelStatus: String,
     val utlånsType: String?,
@@ -25,38 +25,4 @@ data class HjelpemiddelBruker(
     // Berikede felter for bytte
     var kanByttes: Boolean? = null,
     var kanByttesMedBrukerpass: Boolean? = null,
-
-    /*
-        val FOREKOMST_NUMMER: String,
-        val ORDRE_NUMMER: String,
-
-        val ANTALL: String,
-
-        val KATEGORI3_NUMMER: String,
-        val KATEGORI3_BESKRIVELSE: String,
-
-        val ARTIKKELSTATUS: String,
-        val FØRSTE_UTSENDELSE: String,
-
-        val SERIE_NUMMER: String,
-
-        val ARTIKKEL_BESKRIVELSE: String,
-        val ARTIKKELNUMMER: String,
-        val ENHET: String,
-
-        val BRUKER_NUMMER: String,
-        val FNR: String,
-
-        val EGEN_ANSATT: String,
-
-        val INSTALLASJONS_ADDRESSE: String,
-        val INSTALLASJONS_KOMMUNE: String,
-        val INSTALLASJONS_POSTNUMMER: String,
-        val INSTALLASJONS_BY: String,
-
-        val BOSTEDS_ADDRESSE: String,
-        val BOSTEDS_KOMMUNE: String,
-        val BOSTEDS_POSTNUMMER: String,
-        val BOSTEDS_BY: String,
-     */
 )
