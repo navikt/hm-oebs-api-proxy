c0258ea398134d8f0dfc441cd1edd957edb8bdac
Erik Eiesland <erik.eiesland@nav.no>
2024-06-06 13:13:09 +0200

    mer logging under test

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,10 @@ kotlin { jvmToolchain(21) }
 tasks.test {
     useJUnitPlatform()
     testLogging {
+        showCauses = true
+        showExceptions = true
+        showStackTraces = true
+        showStandardStreams = true
         events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
     }
 }
