194e271150bc752e948642647780517e662f12aa
matsbyfl <mats@byfuglien.net>
2024-09-19 15:13:27 +0200

    Reverter isalive

--- src/main/kotlin/no/nav/hjelpemidler/RoutesInternal.kt
+++ src/main/kotlin/no/nav/hjelpemidler/RoutesInternal.kt
@@ -1,6 +1,7 @@
 package no.nav.hjelpemidler
 
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
@@ -12,22 +13,22 @@ import no.nav.hjelpemidler.metrics.Prometheus
 fun Route.internal(database: Database) {
     get("/isalive") {
         // Let's check if the database has been closed
-        // if (database.isClosed) {
-        //    call.respondText("NOT ALIVE", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
-        // } else {
-        call.respondText("ALIVE", ContentType.Text.Plain)
-        // }
+        if (database.isClosed) {
+            call.respondText("NOT ALIVE", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
+        } else {
+            call.respondText("ALIVE", ContentType.Text.Plain)
+        }
     }
 
     get("/isready") {
         // Let's check if the database is still valid and working
-        //  if (!database.isValid()) {
-        // /      Prometheus.oebsDbAvailable.set(0)
-        //    call.respondText("NOT READY", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
-        // } else {
-        //    Prometheus.oebsDbAvailable.set(1)
-        call.respondText("READY", ContentType.Text.Plain)
-        //  }
+        if (!database.isValid()) {
+            Prometheus.oebsDbAvailable.set(0)
+            call.respondText("NOT READY", ContentType.Text.Plain, HttpStatusCode.ServiceUnavailable)
+        } else {
+            Prometheus.oebsDbAvailable.set(1)
+            call.respondText("READY", ContentType.Text.Plain)
+        }
     }
 
     get("/metrics") {
