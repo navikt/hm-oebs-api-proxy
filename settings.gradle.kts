b36c226d8d466ffcdacebd6c30cdab99d5eb227f
matsbyfl <mats@byfuglien.net>
2024-09-24 14:32:58 +0200

    Siste versjon av hotlibs

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            from("no.nav.hjelpemidler:katalog:24.261.093355")
+            from("no.nav.hjelpemidler:katalog:24.268.125256")
         }
     }
 }
