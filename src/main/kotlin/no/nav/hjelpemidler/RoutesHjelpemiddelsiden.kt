00cd29b735d7c390650cdf25423d8a1e053c8195
Håkon <hakonhagen2@gmail.com>
2025-02-04 13:55:56 +0100

    fjern ubrukt kode

--- src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
+++ src/main/kotlin/no/nav/hjelpemidler/RoutesHjelpemiddelsiden.kt
@@ -10,7 +10,6 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import no.nav.hjelpemidler.configuration.Environment
 import no.nav.hjelpemidler.database.Database
-import no.nav.hjelpemidler.models.HjelpemiddelBruker
 
 private val log = KotlinLogging.logger {}
 
@@ -25,24 +24,8 @@ fun Route.hjelpemiddelsiden(database: Database) {
                 log.info { "Processing request for /hjelpemidler-bruker" }
             }
 
-            // Test for å returnere et bytte fordi OeBS ikke klarer å legge inn et
-            val hjelpemiddeloversikt: List<HjelpemiddelBruker> = if (isNotProd() && fnr.toString() == "13820599335") {
-                listOf(
-                    HjelpemiddelBruker(
-                        antall = "1",
-                        antallEnhet = "stk",
-                        kategoriNummer = "183015",
-                        kategori = "Terskelelminator",
-                        artikkelBeskrivelse = "",
-                        artikkelNr = "014123",
-                        serieNr = null,
-                        datoUtsendelse = "",
-                    ),
-                )
-            } else {
-                database.transaction {
-                    hjelpemiddeloversiktDao.hentHjelpemiddeloversikt(fnr.value)
-                }
+            val hjelpemiddeloversikt = database.transaction {
+                hjelpemiddeloversiktDao.hentHjelpemiddeloversikt(fnr.value)
             }
 
             call.respond(hjelpemiddeloversikt)
