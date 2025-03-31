2f5ff9ced1bf89d24070fbadfb337fbeaa7a6dab
Ole Steinar Lillestøl Skrede <ole.steinar.lillestol.skrede@nav.no>
2025-03-31 13:22:33 +0200

    legg til antallDelerPåLager

--- src/main/kotlin/no/nav/hjelpemidler/service/LagerDao.kt
+++ src/main/kotlin/no/nav/hjelpemidler/service/LagerDao.kt
@@ -1,14 +1,17 @@
 package no.nav.hjelpemidler.service
 
 import no.nav.hjelpemidler.database.JdbcOperations
+import no.nav.hjelpemidler.database.Row
 import no.nav.hjelpemidler.database.sql.Sql
 
 class LagerDao(private val tx: JdbcOperations) {
     fun hentLagerstatus(hmsnr: String): List<Lagerstatus> = hentLagerstatus(listOf(hmsnr), null)
 
-    fun hentLagerstatusForSentral(enhetNavn: String, hmsnr: String): Lagerstatus? = hentLagerstatus(listOf(hmsnr), enhetNavn).firstOrNull()
+    fun hentLagerstatusForSentral(enhetNavn: String, hmsnr: String): Lagerstatus? =
+        hentLagerstatus(listOf(hmsnr), enhetNavn).firstOrNull()
 
-    fun hentLagerstatusForSentral(enhetNavn: String, hmsnrs: List<String>): List<Lagerstatus>? = hentLagerstatus(hmsnrs, enhetNavn)
+    fun hentLagerstatusForSentral(enhetNavn: String, hmsnrs: List<String>): List<Lagerstatus>? =
+        hentLagerstatus(hmsnrs, enhetNavn)
 
     private fun hentLagerstatus(hmsnrs: List<String>, orgNavn: String? = null): List<Lagerstatus> {
         var indexedHmsnrs = hmsnrs.withIndex()
@@ -45,17 +48,17 @@ class LagerDao(private val tx: JdbcOperations) {
             sql,
             mapOf("orgNavn" to orgNavn) + indexedHmsnrs.map { (index, hmsnr) -> "hmsnr_$index" to hmsnr },
         ) { row ->
+            val antallPåLager = (row.intOrZero("fysisk") +
+                    row.intOrZero("bestillinger") +
+                    row.intOrZero("anmodning") +
+                    row.intOrZero("intanmodning") -
+                    row.intOrZero("behovsmeldt") -
+                    row.intOrZero("reservert") -
+                    row.intOrZero("restordre"))
+
             Lagerstatus(
-                erPåLager = (
-                    (row.intOrNull("fysisk") ?: 0) +
-                        (row.intOrNull("bestillinger") ?: 0) +
-                        (row.intOrNull("anmodning") ?: 0) +
-                        (row.intOrNull("intanmodning") ?: 0)
-                    ) > (
-                    (row.intOrNull("behovsmeldt") ?: 0) +
-                        (row.intOrNull("reservert") ?: 0) +
-                        (row.intOrNull("restordre") ?: 0)
-                    ),
+                antallPåLager = antallPåLager,
+                erPåLager = antallPåLager > 0,
 
                 organisasjons_id = row.intOrNull("organisasjons_id") ?: -1,
                 organisasjons_navn = row.stringOrNull("organisasjons_navn") ?: "<ukjent>",
@@ -80,7 +83,10 @@ class LagerDao(private val tx: JdbcOperations) {
     }
 }
 
+private fun Row.intOrZero(columnLabel: String): Int = this.intOrNull(columnLabel) ?: 0
+
 data class Lagerstatus(
+    val antallPåLager: Int,
     val erPåLager: Boolean,
 
     val organisasjons_id: Int,
