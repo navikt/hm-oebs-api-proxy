f99c72dca14165dddf77c5033001e0a13b121026
Zandra7 <sandra.eng@live.no>
2025-08-13 13:20:49 +0200

    Legger til hardkodet verdi for garantidato

--- src/main/kotlin/no/nav/hjelpemidler/models/Utlån.kt
+++ src/main/kotlin/no/nav/hjelpemidler/models/Utlån.kt
@@ -1,8 +1,11 @@
 package no.nav.hjelpemidler.models
 
+import java.time.LocalDate
+
 data class Utlån(
     val fnr: String,
     val artnr: String,
     val serienr: String,
     val utlånsDato: String,
+    val garantidato: LocalDate? = LocalDate.now().minusMonths(6)
 )
